6a633985bec5df5e5ab4dbb514ef0d4d166b8acf
Anthony <anthony@amazastrophic.com>
2023-06-22 23:25:09 -0400

    Latest

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,24 @@
 <script>
 	import { browser } from '$app/environment';
 	import { page } from '$app/stores';
-	import { webVitals } from '$lib/vitals';
-	import Header from './Header.svelte';
+
 	import './styles.css';
 
 	/** @type {import('./$types').LayoutServerData} */
 	export let data;
 
 	$: if (browser && data?.analyticsId) {
-		webVitals({
-			path: $page.url.pathname,
-			params: $page.params,
-			analyticsId: data.analyticsId
-		});
+		
 	}
 </script>
 
 <div class="app">
-	<Header />
+	
 
 	<main>
 		<slot />
 	</main>
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
 </div>
 
 <style>
@@ -45,23 +37,5 @@
 		max-width: 64rem;
 		margin: 0 auto;
 		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,199 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import { onMount } from 'svelte';
+	import bgVideo from '$lib/images/front.mp4';
+	import amazacorp from '$lib/images/amazacorp.png';
+	let offset = 0;
+	onMount(() => {
+		offset = window.pageYOffset;
+	});
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Amazastrophic CORP- AI Metaverse Blockchain</title>
+	<meta name="description" content="Amazastrophic CORP is building an immersive multiplayer Unity metaverse, utilizes state-of-the-art Language Learning Models such as OpenAI to create lifelike NPCs. Experience a dynamic world shaped by the permanence of blockchain. Join the next step of game tech today." />
+	<meta name="keywords" content="Metaverse, Multiplayer, Unity, Gaming, OpenAI, AI NPCs, Blockchain, Sage Towers, GameTech, Amazastrophic, LLM, Stable Diffusion, Living NPC">
 </svelte:head>
+<div id="background-video">
+	<video autoplay loop muted playsinline src= {bgVideo} >
+	</video>
+</div>
+<main>
+		<section class="intro">		
+			<h1>Welcome to the cutting edge of gametech with Amazastrophic CORP</h1>
+		<img src={amazacorp} alt="Amazastrophic CORP" class="logo"/>
+		</section>
+		<section class="intro">
+			<h1>Check out our metaverse and start building:</h1>
+			<a href="https://sagetowers.com">
+			<img src="https://sagetowers.com/media/logo.png" alt="Welcome" class="welcome-image" /></a>		
+		</section>
+		<section class="info-section">
+			<p><strong class="highlight">Amazastrophic CORP</strong> is a visionary tech startup dedicated to revolutionizing the metaverse gaming experience by developing a modular blockchain platform that enables metaverses to transition into self-sustaining, player-driven organizations, ultimately creating long-term Decentralized Autonomous Organizations (DAOs). Our mission is to empower players and communities by fostering a decentralized, immersive, and engaging gaming environment.</p>
+			<h2>Our Approach:</h2>
+			<p>At the core of our approach is the integration of cutting-edge technologies, such as Large Language Models (LLMs), to enhance the gaming experience. By leveraging the power of Unity and open-source plugins, we create captivating 3D multiplayer experiences that redefine the boundaries of traditional gaming.</p>
+			<h2>Commitment to Decentralization:</h2>
+			<p>In line with our commitment to decentralization and community-driven development, we aim to utilize a Creative Commons Zero (CC0) license for as much content as possible. This approach aligns with the ethos of a DAO, ensuring that our creations are accessible and adaptable by the community.</p>
+			<h2>Revenue Model:</h2>
+			<p><strong class="highlight">Amazastrophic's</strong> revenue model is centered around offering an array of premium services that cater to various aspects of the metaverse. These services include special edition game skins, titles, and other classic in-app purchase (IAP) gameplay loops. Additionally, we plan to retain any assets generated during our involvement in the creation of DAOs, further diversifying our revenue streams.</p>
+			<h2>Vision:</h2>
+			<p>By combining our passion for gaming with our excitment for cutting-edge AI and blockchain technology, <strong class="highlight">Amazastrophic</strong> is poised to transform the metaverse landscape, delivering unparalleled gaming experiences and empowering players to shape the future of their virtual worlds.</p>
+			<h2>Meet the Founders:</h2>
+			<p>Diamond elo gamers and WoW guild leaders, our founders (<a href="https://twitter.com/eckertanthony">@eckertanthony</a> & <a href="https://twitter.com/TRASHENSTEIN">@TRASHENSTEIN</a>) are on a mission to revitalize the massively multiplayer gaming world.</p>
+			<h1>- - - - - - - - - - - - - - - - - - - - - - - - - - - -</h1>
+			<h1>
+			Private Placement Offering &
+			</h1>
+			<h1>Accredited Investors Only</h1>
+			<p> 
+			Please be advised that the securities offered herein are private placements. They have not been registered under the Securities Act of 1933, but are exempt under Rule 506(c) of Regulation D.
+		    </p>
+			<p>This investment opportunity is limited to accredited investors. As defined by Rule 506(c) of Regulation D, accredited investors must meet certain income or net worth criteria. If you're unsure about your status, we encourage you to consult with your financial advisor.
+			</p>
+			<h1>- - - - - - - - - - - - - - - - - - - - - - - - - - - -</h1>
+			<h1 id="updated-risk-disclosure-">Risk Disclosure</h1>
+			<p>Investing in a high-tech startup, such as Amazastrophic, the initial builders of Sage Towers, carries inherent risks. Potential investors need to be aware of these risks and make informed decisions. Below, we outline the potential risks associated with investing in Amazastrophic:</p>
+			<h2 id="market-and-competition-">Market and Competition:</h2>
+			<p>The gaming and metaverse industries are not just highly competitive but also rapidly evolving. With numerous established players and new startups entering the scene frequently, Amazastrophic faces the challenge of differentiating itself and securing a meaningful market share. While we strive for success, there is always a risk of not achieving our growth and market position targets due to market dynamics, competitive pressure, or changes in user preferences.</p>
+			<h2 id="technological-complexity-">Technological Complexity:</h2>
+			<p>Our work involves integrating cutting-edge technologies, like LLM (Large Learning Models) API&#39;s provided by companies such as OpenAI, which inherently carry the risk of developmental delays, increased costs, or unforeseen challenges. These could impact the timelines and success of our projects.</p>
+			<h2 id="modular-blockchain-platform-development-">Modular Blockchain Platform Development:</h2>
+			<p>While our goal is to build a modular blockchain platform to help metaverses transition into self-sustaining player-run organizations, there is a risk associated with this ambitious goal. It could face technical, regulatory, or other unforeseen obstacles hindering progress or even preventing the realization of this vision.</p>
+			<h2 id="long-term-dao-creation-">Long-term DAO Creation:</h2>
+			<p>Our long-term approach might not align with the expectations of investors seeking quicker returns. Also, the success of transitioning each game into a DAO isn&#39;t guaranteed. Despite our best efforts, some projects might not achieve the desired level of decentralization and community governance, which could affect investor returns.</p>
+			<h2 id="regulatory-and-legal-risks-">Regulatory and Legal Risks:</h2>
+			<p>As we operate in the tech, gaming, and blockchain spaces, we are subject to ever-changing regulatory and legal requirements. While we aim to ensure compliance, there is a risk of facing legal challenges or regulatory scrutiny that could affect our operations and growth.</p>
+			<h2 id="financial-risks-">Financial Risks:</h2>
+			<p>Like any startup, there is a risk that we may not achieve profitability or generate sufficient revenue to sustain our operations. Additionally, initial valuations might not reflect the actual value of the company, especially in a volatile market. Investors must be prepared for the possibility that they may not recoup their initial investment or achieve their desired return.</p>
+			<h2 id="risk-of-dependence-on-key-personnel-">Risk of Dependence on Key Personnel:</h2>
+			<p>Our success heavily depends on the skills, experience, and efforts of our key personnel. If we lose any of these members or fail to attract and retain skilled personnel, it could adversely affect our operations and growth.</p>
+			<h2 id="intellectual-property-risks-">Intellectual Property Risks:</h2>
+			<p>While we strive to protect our intellectual property rights, there may be instances of unauthorized use, which could negatively impact our competitive position and business.</p>
+			<p><em>By understanding these risks, potential investors can make more informed decisions about their involvement with Amazastrophic and the Sage Towers project. We remain committed to addressing these risks diligently and striving towards our vision of revolutionizing the metaverse gaming experience and creating self-sustaining, player-driven organizations.</em></p>
+			<h1>- - - - - - - - - - - - - - - - - - - - - - - - - - - -</h1>
+			<h1 id="business-plan">Business Plan</h1>
+			<h2 id="i-executive-summary">I. Executive Summary</h2>
+			<p>Amazastrophic is a visionary tech startup dedicated to revolutionizing the metaverse gaming experience by developing a modular blockchain platform that enables metaverses to transition into self-sustaining, player-driven organizations, ultimately creating long-term Decentralized Autonomous Organizations (DAOs). Our mission is to empower players and communities by fostering a decentralized, immersive, and engaging gaming environment.</p>
+			<h2 id="ii-company-overview">II. Company Overview</h2>
+			<p>Amazastrophic is a technology-driven company focused on creating innovative gaming experiences within the metaverse. Our team consists of experienced professionals with expertise in cutting-edge technologies, blockchain, and game development. We are committed to building a strong, engaged community that feels a sense of ownership and involvement in the project.</p>
+			<h2 id="iii-products-and-services">III. Products and Services</h2>
+			<h3 id="sage-towers-metaverse-">Sage Towers Metaverse:</h3>
+			<p>An immersive gaming world where players can explore new dimensions, gather resources, craft items, and interact with other players in real time.</p>
+			<h3 id="psypher-ai-">Psypher AI:</h3>
+			<p>Represents the &quot;sentient&quot; metaverse AI, &quot;living NPC&quot;, that enhances the gaming experience by providing personalized interactions and adapting to player behavior.</p>
+			<h3 id="modular-blockchain-platform-">Modular Blockchain Platform:</h3>
+			<p>A platform designed to help metaverses transition into self-sustaining organizations run by players, creating long-term DAOs. L3 chains that minimize friction for the end user.</p>
+			<h3 id="premium-services-">Premium Services:</h3>
+			<p>An array of premium services, including special edition game skins, titles, and other classic in-app purchase (IAP) gameplay loops.</p>
+			<h2 id="iv-marketing-plan">IV. Marketing Plan</h2>
+			<h3 id="target-audience-">Target Audience:</h3>
+			<p>Gamers and metaverse enthusiasts seeking immersive, decentralized gaming experiences.</p>
+			<h3 id="channels-">Channels:</h3>
+			<p>Social media, gaming forums, Discord, and influencer partnerships.</p>
+			<h3 id="community-engagement-">Community Engagement:</h3>
+			<p>Regular updates, community events, and feedback channels to foster a strong, engaged community.</p>
+			<h3 id="branding-">Branding:</h3>
+			<p>A strong brand identity that reflects the innovative and decentralized nature of Amazastrophic games.</p>
+			<h2 id="v-sales-strategy">V. Sales Strategy</h2>
+			<h3 id="freemium-model-">Freemium Model:</h3>
+			<p>Offer a free-to-play experience with optional premium services to cater to various player preferences.</p>
+			<h3 id="in-game-purchases-">In-Game Purchases:</h3>
+			<p>Generate revenue through in-app purchases, such as special edition game skins, titles, and other gameplay loops.</p>
+			<h3 id="asset-retention-">Asset Retention:</h3>
+			<p>Retain any assets generated during our involvement in the creation of DAOs, further diversifying our revenue streams.</p>
+			<h2 id="vi-financial-projections">VI. Financial Projections</h2>
+			<h3 id="revenue-growth-">Revenue Growth:</h3>
+			<p>Achieve sustainable revenue growth through a combination of premium services and asset retention.</p>
+			<h3 id="profitability-">Profitability:</h3>
+			<p>Reach profitability by optimizing development costs and maximizing revenue from in-game purchases and asset retention.</p>
+			<h3 id="long-term-financial-stability-">Long-term Financial Stability:</h3>
+			<p>Establish a solid financial foundation to support the ongoing development and growth of all Amazastrophic metaverses.</p>
+			<h2 id="vii-risk-management">VII. Risk Management</h2>
+			<h3 id="market-and-competition-">Market and Competition:</h3>
+			<p>Differentiate Amazastrophic metaverses from competitors by offering a unique, LLM AI focused, decentralized gaming experience.</p>
+			<h3 id="technological-complexity-">Technological Complexity:</h3>
+			<p>Continuously invest in research and development to overcome technical challenges and stay ahead of industry trends.</p>
+			<h3 id="regulatory-and-legal-risks-">Regulatory and Legal Risks:</h3>
+			<p>Monitor and adapt to changing regulations and legal requirements to ensure compliance and mitigate potential risks.</p>
+			<p>By following this business plan, Amazastrophic aims to achieve its vision of revolutionizing the metaverse gaming experience and creating self-sustaining, player-driven organizations that empower players and communities to shape the future of their virtual worlds.</p>
+		</section>
+		<h1>- - - - - - - - - - - - - - - - - - - - - - - - - - - -</h1>
+		<h1 id="fundraising-goals">Fundraising Goals</h1>
+		<p>In order to bring our vision to life and set the stage for sustainable growth, we are currently raising funds. Our goal is to secure a total investment of $2,500,000 which will provide us with a comfortable runway of five years based on our projected expenses. We believe that will allow us to transition our project to full DAO</p>
+		<h2 id="development-">Development:</h2>
+		<p>From enhancing our AI technology to ensuring smooth gameplay in our metaverse, your investment will help us build an immersive and interactive gaming experience.</p>
+		<h2 id="legal-">Legal:</h2>
+		<p>As we navigate the complex landscape of game development and blockchain technology, legal support is critical to ensure that we are compliant with all relevant laws and regulations.</p>
+		<h2 id="community-">Community:</h2>
+		<p>We are committed to building a dedicated community of players who will shape the future of our metaverse. Your investment will enable us to foster this community and provide the necessary support.</p>
+		<h1>- - - - - - - - - - - - - - - - - - - - - - - - - - - -</h1>
+	    <h1 id="disclaimer-">Disclaimer</h1>
+		<h2 id="forward-looking-statements">Forward-Looking Statements:</h2>
+		<p>This website may contain forward-looking statements that reflect our current expectations and projections about Amazastrophic&#39;s future results, performance, prospects, and opportunities. These forward-looking statements are based on information currently available to us and are subject to a number of risks, uncertainties, and other factors that could cause our actual results, performance, prospects, or opportunities to differ materially from those expressed in, or implied by, these forward-looking statements. Actual results may differ significantly from those stated or implied in forward-looking statements due to factors such as changes in the regulatory environment, fluctuations in the market, and the performance of our products. These forward-looking statements are subject to the &#39;safe harbor&#39; provisions of the Private Securities Litigation Reform Act of 1995, which limits potential liabilities related to such statements.</p>
+		<h2 id="investment-disclaimer">Investment Disclaimer:</h2>
+		<p>The information provided on this website, or any content linked from this website, does not constitute investment advice, financial advice, trading advice, or any other sort of advice, and you should not treat any of the website&#39;s content as such. Amazastrophic does not recommend that any cryptocurrency, security, or other investment should be bought, earned, sold, or held by you. We strongly suggest conducting your own due diligence or consulting with a qualified financial advisor before making any investment decisions.</p>
+		<h2 id="accuracy-of-information">Accuracy of Information:</h2>
+		<p>Amazastrophic strives to ensure that the information on this website is updated and correct. However, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability, or availability concerning the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is strictly at your own risk.</p>
+		<h2 id="risk-statement">Risk Statement:</h2>
+		<p>Investing in digital assets, such as the ones mentioned on this website, is risky and speculative. Investors should understand that their capital is at risk and they may potentially lose all of their investment. The digital assets&#39; value and performance can be highly volatile and could rapidly change. Past performance is not indicative of future results.</p>
+		<h2 id="securities-disclosure">Securities Disclosure:</h2>
+		<p>If Amazastrophic ever offers any securities for sale, such offers would be private placements, exempt from registration under the Securities Act of 1933 pursuant to Rule 506(c) of Regulation D. Such offers would only be made in compliance with applicable state and federal securities laws and would be available only to qualified accredited investors. Any such offer would be made only through appropriate disclosure documents and only in jurisdictions where it is lawfully permitted.</p>
+		<h2 id="state-securities-and-blue-sky-laws">State Securities and &#39;Blue Sky&#39; Laws:</h2>
+		<p>Offers and sales of securities by Amazastrophic will only be conducted in compliance with state securities or &#39;Blue Sky&#39; laws to the extent applicable, and investors are advised to become aware of and understand these laws before investing.</p>
+		<h1>- - - - - - - - - - - - - - - - - - - - - - - - - - - -</h1>
+		<footer>
+			<p>Interested in investing? <a href="mailto:cs@amazastrophic.com">Contact us</a> to learn more.</p>
+			<div class="socials"><a href="https://discord.gg/sagetowers">Discord</a></div>
+		</footer>
+	</main>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
+	<style>
+		#background-video {
+		position: fixed;
+		top: 0;
+		left: 0;
 		width: 100%;
 		height: 100%;
-		top: 0;
-		display: block;
+		z-index: -1;
+		overflow: hidden;
 	}
+
+	#background-video video {
+		object-fit: cover;
+		width: 100%;
+		height: 100%;
+	}
+		main {
+			max-width: 800px;
+			margin: 0 auto;
+			padding: 1em;
+			background-color: hsla(0, 0%, 23%, 0.961);
+			color: white;
+		}
+	
+		.intro {
+			text-align: center;
+			padding: 2em 0;
+			color: white;
+		}
+	
+		.welcome-image {
+			max-width: 70%;
+			height: auto;
+			margin-bottom: 1em;
+		}
+
+		footer {
+			padding: 1em;	
+			color: white;
+			text-align: center;
+		}
+
+		.logo {
+			width: 210px;
+			transition: filter 0.3s ease-in-out;
+		}
+		.logo:hover {
+			filter: invert(1);
+		}
+
 </style>
